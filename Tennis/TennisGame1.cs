1e5a4e86740830152fa0be4c7bb6cbaf1b2da3e8
TomMir <tommaso.miranda@gmail.com>
2017-02-02 12:01:29 +0100

    primo refactoring al metodo

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -68,19 +68,14 @@ namespace Tennis
             switch (_mScore1)
             {
                 case 0:
-                    score = "Love-All";
-                    break;
+                    return "Love-All";
                 case 1:
-                    score = "Fifteen-All";
-                    break;
+                    return "Fifteen-All";
                 case 2:
-                    score = "Thirty-All";
-                    break;
+                    return "Thirty-All";
                 default:
-                    score = "Deuce";
-                    break;
+                    return "Deuce";
             }
-            return score;
         }
     }
 }
