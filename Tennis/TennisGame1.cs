9a99872f1914790d2827d3896d06c14ae45482d6
TomMir <tommaso.miranda@gmail.com>
2017-02-02 14:56:23 +0100

    Tolti i metodi ormai non usati

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -28,57 +28,6 @@ namespace Tennis
             return _scoreCounter.AsString();
         }
 
-        private string Score()
-        {
-            string score = "";
-            if (_mScore1 == _mScore2)
-            {
-                score = ToStringScore();
-            }
-            else if (_mScore1 >= 4 || _mScore2 >= 4)
-            {
-                score = StringAdvantageScore();
-            }
-            else
-            {
-                score = StringScoreOther(score);
-            }
-            return score;
-        }
-
-        private string StringScoreOther(string score)
-        {
-            for (var i = 1; i < 3; i++)
-            {
-                int tempScore;
-                if (i == 1) tempScore = _mScore1;
-                else
-                {
-                    score += "-";
-                    tempScore = _mScore2;
-                }
-                var storeStringArray = new[] {"Love", "Fifteen", "Thirty", "Forty"};
-                score += storeStringArray[tempScore];
-            }
-            return score;
-        }
-
-        private string StringAdvantageScore()
-        {
-            string score;
-            var minusResult = _mScore1 - _mScore2;
-            if (minusResult == 1) score = "Advantage " + _player1Name;
-            else if (minusResult == -1) score = "Advantage " + _player2Name;
-            else if (minusResult >= 2) score = "Win for " + _player1Name;
-            else score = "Win for " + _player2Name;
-            return score;
-        }
-
-        private string ToStringScore()
-        {
-            var retArray = new[] {"Love-All", "Fifteen-All", "Thirty-All"};
-            return _mScore1<=2 ? retArray[_mScore1] : "Deuce";
-        }
     }
 }
 
